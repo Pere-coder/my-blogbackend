09795ef4dd971461fbd6adc2dd66807ff6a94522
Pere <gpere800@gmail.com>
2023-07-14 11:31:22 -0700

    modified:   blogapi/__pycache__/settings.cpython-311.pyc 	modified:   blogapi/settings.py 	modified:   blogapp/__pycache__/admin.cpython-311.pyc 	modified:   blogapp/admin.py 	new file:   env/Lib/site-packages/corsheaders/__init__.py 	new file:   env/Lib/site-packages/corsheaders/__pycache__/__init__.cpython-311.pyc 	new file:   env/Lib/site-packages/corsheaders/__pycache__/apps.cpython-311.pyc 	new file:   env/Lib/site-packages/corsheaders/__pycache__/checks.cpython-311.pyc 	new file:   env/Lib/site-packages/corsheaders/__pycache__/conf.cpython-311.pyc 	new file:   env/Lib/site-packages/corsheaders/__pycache__/defaults.cpython-311.pyc 	new file:   env/Lib/site-packages/corsheaders/__pycache__/middleware.cpython-311.pyc 	new file:   env/Lib/site-packages/corsheaders/__pycache__/signals.cpython-311.pyc 	new file:   env/Lib/site-packages/corsheaders/apps.py 	new file:   env/Lib/site-packages/corsheaders/checks.py 	new file:   env/Lib/site-packages/corsheaders/conf.py 	new file:   env/Lib/site-packages/corsheaders/defaults.py 	new file:   env/Lib/site-packages/corsheaders/middleware.py 	new file:   env/Lib/site-packages/corsheaders/py.typed 	new file:   env/Lib/site-packages/corsheaders/signals.py 	new file:   env/Lib/site-packages/django_cors_headers-4.2.0.dist-info/INSTALLER 	new file:   env/Lib/site-packages/django_cors_headers-4.2.0.dist-info/LICENSE 	new file:   env/Lib/site-packages/django_cors_headers-4.2.0.dist-info/METADATA 	new file:   env/Lib/site-packages/django_cors_headers-4.2.0.dist-info/RECORD 	new file:   env/Lib/site-packages/django_cors_headers-4.2.0.dist-info/REQUESTED 	new file:   env/Lib/site-packages/django_cors_headers-4.2.0.dist-info/WHEEL 	new file:   env/Lib/site-packages/django_cors_headers-4.2.0.dist-info/top_level.txt

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Form
 
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("email", "date")
+
 # Register your models here.
-admin.site.register(Form)
+admin.site.register(Form, FormAdmin)
 
